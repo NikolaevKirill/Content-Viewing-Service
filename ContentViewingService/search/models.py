5608f465335e710b4ec701d777e669b6f678800a
NikolaevKirill <nikolaev.kirill.p@gmail.com>
2022-02-05 18:33:35 +0300

    Corrections

--- ContentViewingService/search/models.py
+++ ContentViewingService/search/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Images(models.Model):
-    url = models.CharField("url", max_length=250)
-    number_of_show = models.IntegerField("number_of_show")
-    categories = models.CharField("categories", max_length=250)
+    url = models.CharField("url", max_length=250, default = None)
+    number_of_show = models.IntegerField("number_of_show", default = 0)
+    categories = models.CharField("categories", max_length=250, default = None)
 
     def __str__(self):
         return self.url
